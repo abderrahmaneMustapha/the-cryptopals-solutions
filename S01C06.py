11d75232f1bfac9339dd59a488fc35a059b2c438
abderrahmane <abderrahmanemustapha030898@gmail.com>
2019-05-13 16:34:52 +0100

    correct hamming distance

--- S01C06.py
+++ S01C06.py
@@ -1,19 +1,13 @@
-def add_zero(str):
-    if len(str) < 6:
-        str+='0'
-    return str
-
-
 def hamming_distance(str1, str2):
     distance = 0
     if len(str1) == len(str2):
-        str1_bin = ''.join([add_zero(bin(ord(s))[2:]) for s in str1])
-        str2_bin = ''.join([add_zero(bin(ord(s))[2:]) for s in str2])
+        #str1_bin = ''.join([bin(ord(s))[2:] for s in str1])
+        #str2_bin = ''.join([bin(ord(s))[2:] for s in str2])
 
-        for s1 , s2 in zip(str1_bin, str2_bin):
-            if  s1 != s2  :
-                distance += 1
+        for s1 , s2 in zip(str1, str2):
+            temp =  ord(s1)^ ord(s2)
+
+            distance += sum([1  for d in bin(temp) if d=='1'])
     print(distance)
 
-
-hamming_distance('this is a test', 'wokka wokka!!!')
+hamming_distance(b'this is a test', b'wokka wokka!!!')
